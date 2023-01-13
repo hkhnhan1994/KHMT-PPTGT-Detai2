4d46508dd4bb3c6a7ee875f0f6f4424da6eab91c
nhanhuynh <nhan.huynh@unifiedpost.com>
2023-01-13 16:01:15 +0700

    add java and c++

---
+++ LHSN.java
@@ -0,0 +1,5 @@
+public class LHSN{
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main(){
+    uint64_t songuyen=0;
+    float sothuc=0.0;
+    int count=1;
+    while(true){
+        songuyen++;
+        sothuc++;
+        if(songuyen!=sothuc){
+            std::cout << "so nguyen:" <<songuyen<<" so thuc:"<<sothuc<< std::endl;
+            sothuc+=count;
+            count++;
+            std::cout << "counter:" <<count<< std::endl;
+        }
+        else{
+            count=0;
+        }
+
+    }
+    
+}
