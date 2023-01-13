1440b79d747066058a6c06b9228b5d4573483bd4
nhanhuynh <nhan.huynh@unifiedpost.com>
2023-01-13 17:22:05 +0700

    modify method

--- main.cpp
+++ main.cpp
@@ -3,19 +3,27 @@ int main(){
     uint64_t songuyen=0;
     float sothuc=0.0;
     int count=1;
+    int max=1;
     while(true){
-        songuyen++;
-        sothuc++;
+        
         if(songuyen!=sothuc){
-            std::cout << "so nguyen:" <<songuyen<<" so thuc:"<<sothuc<< std::endl;
-            sothuc+=count;
+            sothuc=sothuc+float(count);
             count++;
-            std::cout << "counter:" <<count<< std::endl;
+            if(songuyen==sothuc){
+                sothuc=float(songuyen);
+                if (max<count)
+                {
+                max=count;
+                std::cout << "counter:" <<count<< std::endl;
+                std::cout << "so nguyen:" <<songuyen<<" so thuc:"<<sothuc<< std::endl;
+                }
+            }
+            
         }
         else{
+            songuyen++;
+            sothuc++;
             count=1;
         }
-
     }
-    
 }
